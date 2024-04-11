95b0905e508f0a3a29c4c8bfb96e65d31b08fff2
kauanealvesheleodoro <107581220+kauanealvesheleodoro@users.noreply.github.com>
2024-04-11 17:31:14 -0300

    11-04-2024

--- main.js
+++ main.js
@@ -3,13 +3,31 @@ const textos=document.querySelectorAll(".aba-conteudo")
 //Laço de repetição
 
 for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
+    botoes[i].onclick = function () { 
+
         for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos [j].classList.remove("ativo");
-        }
+        } 
+
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
+} 
+
+const contadores = document.querySelectorAll(".contador")
+const tempoObjetivo1 = new Date ("2024-07-08T000:00:00");
+
+
+contadores[0].textContent= calculaTempo (tempoObjetivo1); 
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date (); 
+
+
+
+
+
+
 }
 
